761710cb9675541a84d553879b0c292813dd17d8
Zack <lorenx@163.com>
2024-05-12 00:49:59 +0800

    chore: update infra

---
+++ packages/db/src/config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "drizzle-kit";
+import { createEnv } from "@t3-oss/env-core";
+import * as z from "zod";
+
+const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+  },
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+});
+
+export const connectionStr = new URL(env.DATABASE_URL);
+
+console.log("connectionStr", connectionStr.href);
+
+export default {
+  schema: "./src/schema",
+  dialect: "postgresql",
+  dbCredentials: { url: connectionStr.href },
+  tablesFilter: ["t3turbo_*"],
+} satisfies Config;
---
+++ packages/db/src/index.ts
@@ -0,0 +1,17 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+import { connectionStr } from "./config";
+import * as auth from "./schema/auth";
+import * as post from "./schema/post";
+
+export * from "drizzle-orm/sql";
+export { alias } from "drizzle-orm/pg-core";
+
+export const schema = { ...auth, ...post };
+
+const psClient = new Pool({
+  connectionString: connectionStr.href,
+});
+
+export const db = drizzle(psClient, { schema, logger: true });
