60c9a58c77a8c94352fca6521ad800fa3724685d
Zack <lorenx@163.com>
2024-05-14 00:49:47 +0800

    feat: update pm2

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,4 +2,13 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-export default app;
+app.get("/", (c) => {
+  return c.json({
+    message: "Hello, World!",
+  });
+});
+
+export default {
+  ...app,
+  port: 3001,
+};
---
+++ pm2.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name: "insonice", // Name of your application
+    script: "bun run start", // Entry point of your application
+  };
