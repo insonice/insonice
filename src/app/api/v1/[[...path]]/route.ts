dbbeda43d3ffe5a4bf100114353d50854e41eab4
Zack <lorenx@163.com>
2025-02-17 18:18:22 +0800

    feat: update types

--- src/app/api/v1/[[...path]]/route.ts
+++ src/app/api/v1/[[...path]]/route.ts
@@ -1,15 +1,12 @@
 import { handle } from "hono/vercel";
 import { createServer } from "@/server/app";
-
 export const runtime = "nodejs";
 
 const prefix = "/api/v1";
 
 const app = createServer(prefix);
 
-const route = app.get("/ping", async c => c.text("pong", 200));
-
 export const GET = handle(app);
 export const POST = handle(app);
 
-export type AppType = typeof route;
+export type AppType = typeof app;
--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,21 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import type { BlankEnv, BlankSchema } from "hono/types";
 
-export const createServer = (prefix: string) => {
+/**
+ * Create a server with a prefix
+ * @param prefix The prefix of the server
+ * @returns The server
+ */
+export function createServer<Prefix extends string>(prefix: Prefix) {
+  // Create the app
   const app = new Hono().basePath(prefix);
+
+  // Middleware
   app.use("*", cors());
-  return app;
-};
+
+  // Routes
+  app.get("/ping", async c => c.text("pong", 200));
+
+  return app as Hono<BlankEnv, BlankSchema, Prefix>;
+}
