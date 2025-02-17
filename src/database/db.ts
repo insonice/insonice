d8767863ff9d49236eaa24b398aa4da5907b1bde
Zack <lorenx@163.com>
2025-02-17 21:26:00 +0800

    feat: update

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,4 +1,15 @@
 import { drizzle } from "drizzle-orm/d1";
+import { getRequestContext } from '@cloudflare/next-on-pages';
 
 // @ts-ignore
-export const db = drizzle(env.DATABASE as any);
+export const db = drizzle(globalThis.env.DATABASE as any);
+
+function initDbConnection() {
+  if (process.env.NODE_ENV === 'development') {
+    const { env } = getRequestContext();
+
+    return drizzle(env.DB, { schema });
+  }
+
+  return drizzle(process.env.DB as unknown as D1Database, { schema });
+}
